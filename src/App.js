576cc7026c535c9189c7e5554e973ea9f6b02af0
Mateo <mgomezossa@gmail.com>
2020-05-12 18:48:46 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
-
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      guests: [],
+      newfirst: '',
+      newlast: '',
+      error: '',
+    }
+  }
+  addUser = (event) => {
+    event.preventDefault();
+    if (this.state.newfirst === '' || this.state.newlast === '') {
+      this.setState({ error: 'error' })
+    } else {
+      let oldTasks = this.state.guests
+      let newTask = { first: this.state.newfirst, last: this.state.newlast };
+      this.setState({
+        guests: [...oldTasks, newTask],
+        newfirst: '',
+        newlast: '',
+      })
+    }
+  }
+  updateFirst = (event) => {
+    this.setState({
+      newfirst: event.target.value,
+    })
+  }
+  updateLast = (event) => {
+    this.setState({
+      newlast: event.target.value,
+    })
+  }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={(event) => this.addUser(event)}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input value={this.state.newfirst} className={this.state.error} type="text" className="form-control" name="first-name" onChange={this.updateFirst.bind(this)} />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input value={this.state.newlast} className={this.state.error} type="text" className="form-control" name="last-name" onChange={this.updateLast.bind(this)} />
               </div>
 
               <div className="action">
@@ -30,7 +62,12 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.state.guests.map((guest) =>
+                  <tr>
+                    <td>{guest.first}</td>
+                    <td>{guest.last}</td>
+                  </tr>)
+                }
               </tbody>
             </table>
           </div>
